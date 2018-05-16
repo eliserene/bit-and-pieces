8ae16b1148450d92a7f70ab21916bb8d88a81ad7
Elise Dettrick <elise@dettrick.net>
2018-05-16 15:12:21 +1000

    Add sample dialoge flow response

--- index.js
+++ index.js
@@ -1,3 +1,9 @@
 exports.helloGET = (req, res) => {
-  res.send('Hello World!');
+  res.send("Hello World!");
+};
+
+exports.helloHttp = function helloHttp(request, response) {
+  response.json({
+    fulfillmentText: "This is a sample response from your webhook!"
+  });
 };
